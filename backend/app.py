dc18e605db8147bee6ffdd994df0db93e6cb4e76
Richard Coucoules <coules93@gmail.com>
2020-09-10 15:08:57 -0700

    Add skeleton RESTful backend in Flask

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Home route
+#   GET returns all journal entries by a user
+#   POST takes a new journal entry and adds it to database
+@app.route("/diarist/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        # TODO: authenticate, return list of all journal entries
+        return jsonify({
+            "route": "/",
+            "http-method": "GET"
+        })
+    elif request.method == 'POST':
+        # TODO: authenticate, verify request body, and add
+        # journal entry to database
+        return request.get_json()
+    # else http method is not in methods parameter
+    # flasks handles this automatically
+
+# Delete route - deletes journal entry by ID
+@app.route("/diarist/delete/<int:id>", methods=['DELETE'])
+def delete(id):
+    # TODO: authenticate, remove journal entry with provided ID
+    return jsonify({
+        "route": "/delete/<id>",
+        "http-method": "DELETE"
+    })
+
+# Edit route - updates a journal entry associated with ID
+@app.route("/diarist/edit/<int:id>", methods=['PUT'])
+def edit(id):
+    # TODO: authenticate, replace journal entry with provided
+    #   ID with supplied data
+    return jsonify({
+        "route": "/edit/<id>",
+        "http-method": "PUT"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
